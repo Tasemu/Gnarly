1382ee8da4b9daba2e02927de070b9b975fcb587
Montague <monty.monro@blackmilkclothing.com>
2015-11-27 10:47:09 +1000

    Adding functions
    * Added MOTD and Territory Timers
    * Established connection to google docs

--- bot.js
+++ bot.js
@@ -1,13 +1,53 @@
-var Discord = require('discord');
+var Discord = require('discord.js');
+var GoogleSpreadsheet = require('google-spreadsheet');
 var config = require('./config.json');
-var bot = new Discord.client();
+var bot = new Discord.Client();
+var spreadsheet = new GoogleSpreadsheet('1H2rDTyrg4g1sGVbiWMW7dL-YEzQ4RJX7eD9dZNbrhHk');
 
 bot.on('ready', function () {
 	console.log('Gnarly is initialised and ready for use!');
 });
 
-bot.on('message', function (message) {});
-bot.on('disconnected', function () {});
-bot.on('error', function (err) {});
+bot.on('message', function (message) {
+
+	if (message.content.substr(0, 1) !== '!') return;
+	var structure = message.content.split(' ');
+	var command = structure[0];
+	var context = structure[1];
+
+	if (command === '!ping') {
+		message.reply('pong');
+	}
+
+	if (command === '!motd') {
+		spreadsheet.getRows(1, function (err, data) {
+			data.forEach(function (row) {
+				if (row.name === "MOTD") message.reply(row.message);
+			});
+		});
+	}
+
+	if (command === '!territories') {
+		spreadsheet.getRows(2, function (err, data) {
+			data.forEach(function (row) {
+				message.reply(row.territory + " :: " + row.timer);
+			});
+		});
+	}
+
+});
+
+bot.on('disconnected', function () {
+	console.log('Disconnected, exiting!');
+	process.exit(1);
+});
+
+bot.on("debug", function (message) {
+	console.log("Gnarly Debug Message: " + message);
+});
+
+bot.on("error", function (error) {
+	console.log("Error Caught: " + error);
+});
 
 bot.login(config.auth.email, config.auth.password);
